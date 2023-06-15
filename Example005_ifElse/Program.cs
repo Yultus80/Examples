ad99f90d3163859e23799d291d7cd0e2f516adcb
Yultus80 <yultus@yandex.ru>
2023-06-15 17:42:10 +0300

    0051

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Привет чувиха");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
