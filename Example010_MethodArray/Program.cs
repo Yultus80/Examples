61389dc38e1d850bf6fc2bbaf57fbc81bd1bbfcd
Yultus80 <yultus@yandex.ru>
2023-06-16 19:17:11 +0300

    10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 14, 12, 31, 45, 18, 15, 16, 17, 18 };
+
+int n = array.Length;  //показать какой номер в массиве у определенного числа
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;  //остановит выполнение программы
+    }
+    index++;
+
+}
